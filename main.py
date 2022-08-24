b894f5ca264f5e35719d9f4b34b0365a0ff5205c
Saifeddine Bejaoui <6457481-Saifeddineb@users.noreply.gitlab.com>
2022-08-25 00:14:42 +0200

    Add user DELETE and PUT Request

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from typing import Optional, List
-from models import User, Gender, Role
-from fastapi import FastAPI
+from models import User, Gender, Role, UserUpdateRequest
+from fastapi import FastAPI, HTTPException
 from uuid import UUID, uuid4
 
 app = FastAPI()
@@ -12,15 +12,14 @@ db: List[User] = [
     last_name="Ahmed",
     gender=Gender.female,
     roles=[Role.admin, Role.user]
-),
+    ),
 
-User(id=UUID("7b78a343-e5c2-4d76-86db-0ee48c6b9a49"),
-    first_name="Alex",
-    last_name="Jones",
-    gender=Gender.male,
-    roles=[Role.admin, Role.user]
-)
-
+    User(id=UUID("7b78a343-e5c2-4d76-86db-0ee48c6b9a49"),
+        first_name="Alex",
+        last_name="Jones",
+        gender=Gender.male,
+        roles=[Role.admin, Role.user]
+    )
 ]
 # GET HTTP Request
 @app.get("/")
@@ -32,8 +31,39 @@ async def root():
 async def fetch_users():
     return db;
 
-
 @app.post('/api/v1/users')
 async def register_user(user: User):
     db.append(user)
     return {'id', user.id}
+
+
+@app.delete('/api/v1/users/{user_id}')
+async def delete_user(user_id: UUID):
+    for user in db:
+        if user.id == user_id:
+            db.remove(user)
+            return
+    
+    raise HTTPException(
+        status_code=404,
+        detail=f'user with id: "{user_id}" does not exists'
+    )
+
+@app.put("/api/v1/users/{user_id}")
+async def update_user(user_update: UserUpdateRequest, user_id: UUID):
+    for user in db:
+        if user.id == user_id:
+            if user_update.first_name is not None:
+                user.first_name = user_update.first_name
+            if user_update.last_name is not None:
+                user.last_name = user_update.last_name
+            if user_update.middle_name is not None:
+                user.middle_name = user_update.middle_name
+            if user_update.roles is not None:
+                user.roles = user_update.roles 
+            return
+    
+    raise HTTPException(
+        status_code=404,
+        detail=f'user with id "{user_id}" does not exists'
+    )
